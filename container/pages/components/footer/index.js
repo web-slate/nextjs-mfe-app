ec718cb17d6c9a14e9f293ffee93807a03735eec
Venkat.R <ramsunvtech@gmail.com>
2021-01-03 23:25:57 +0800

    feat(container): common components

---
+++ container/pages/components/footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from '../../../styles/Home.module.css'
+
+export default function Footer() {
+  return (
+    <header className={styles.footer}>
+      <h1>Container Header</h1>
+    </header>
+  );
+}
---
+++ container/pages/components/header/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from '../../../styles/Home.module.css'
+
+export default function Header() {
+  return <footer className={styles.footer}>Container Footer</footer>;
+}
