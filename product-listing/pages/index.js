271fddd04434179858f52c4e5d4f9cd7e44fac4b
Venkat.R <ramsunvtech@gmail.com>
2021-01-05 13:53:18 +0800

    feat(plp): Add PLP Index Page

--- product-listing/pages/index.js
+++ product-listing/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/plp.module.css'
 
-export default function Home() {
+export default function ProductListing() {
   return (
     <div className={styles.container}>
       <Head>
